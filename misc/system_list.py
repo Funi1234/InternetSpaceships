fef73ced8de055038cef0a640db0ff629bb08e2d
Dylan Conway <funi1234@gmail.com>
2013-07-31 09:28:20 +0100

    Updated SystemID - System Name map
    added in header
    added comments
    reverse lookup functionality

--- misc/system_list.py
+++ misc/system_list.py
@@ -1,17 +1,24 @@
+####################################################################################################
+# @author Dylan Conway https://github.com/Funi1234/InternetSpaceships
+# @description Queries the Eve Online API and compiles a list of systemID to system name mappings
+#
+# @date 2013-07-30
+####################################################################################################
+
 import lxml
 import json
 import urllib
 from lxml import etree
 
+# Get the XML from API
 tmpresponce = urllib.urlopen("https://api.eveonline.com/map/Sovereignty.xml.aspx")
 
+#XML To String
 resString = tmpresponce.read()
 
-#print type(resString)
-
-
+# Create XML Parser.
 parser = etree.XMLParser(remove_blank_text=True)
-
+# Parse String into XML Object
 responce = etree.XML(resString, parser)
 
 print "starting"
@@ -26,6 +33,13 @@ for row in rows[0]:
     systemIDMap[row.get("solarSystemID")] = row.get("solarSystemName")
     
 
-
+# Print the system name for that ID
 print systemIDMap["30002768"]
+
+# Print the ID for that system name
+for sysid, sys in systemIDMap.iteritems():
+    if sys.startswith("NOL") > 0:
+        print sysid
+
+        
 #print etree.tostring(responce, pretty_print=True)
