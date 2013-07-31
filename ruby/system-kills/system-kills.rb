cbc0d06463985e8dad30f9bfb0f2de6b2d052f7a
David Kirwan <dkirwan@tssg.org>
2013-07-31 15:50:23 +0100

    - Added system-kills script which polls the API and retrieves   information about kills, faction kills and pod kills

--- ruby/system-kills/system-kills.rb
+++ ruby/system-kills/system-kills.rb
@@ -1,11 +1,47 @@
 ####################################################################################################
 # @author David Kirwan https://github.com/davidkirwan/ardtweeno
 # @description Queries the Eve Online API and retrieves information about system kills
+# 
+# @APIURI https://api.eveonline.com/map/Kills.xml.aspx
 #
 # @date 2013-07-31
 ####################################################################################################
 
+require 'json'
 require 'nokogiri'
+require 'open-uri'
+require "../system-sovereignty/system-sovereignty.rb"
 
+def killlist()
+  sovlist = sovlist()
+  killlist = Array.new
 
+  kills = Nokogiri::HTML(open("https://api.eveonline.com/map/Kills.xml.aspx"))
+
+  rows = kills.xpath("//row")
+
+  rows.each do |r|
+    key = r.attr("solarsystemid")
+    val = nil
+
+    sovlist.each do |i|
+      unless i[key].nil? then val = i[key]; end
+    end
+
+    kill = r.attr("shipkills")
+    factionkills = r.attr("factionkills")
+    pods = r.attr("podkills")
+
+    hash = Hash.new
+
+    hash[key] = val
+    hash["shipkills"] = kill
+    hash["factionkills"] = factionkills
+    hash["podkills"] = pods
+
+    killlist << hash
+  end
+
+  return killlist
+end
 
--- ruby/system-sovereignty/system-sovereignty.rb
+++ ruby/system-sovereignty/system-sovereignty.rb
@@ -11,25 +11,25 @@ require 'json'
 require 'nokogiri'
 require 'open-uri'
 
-systemlist = Array.new
+def sovlist()
+  systemlist = Array.new
 
-sov = Nokogiri::HTML(open("https://api.eveonline.com/map/Sovereignty.xml.aspx"))
+  sov = Nokogiri::HTML(open("https://api.eveonline.com/map/Sovereignty.xml.aspx"))
 
-rows = sov.xpath("//row")
+  rows = sov.xpath("//row")
 
-rows.each do |r|
-  key = r.attr("solarsystemid")
-  val = r.attr("solarsystemname")
-  faction = r.attr("factionid")
-  alliance = r.attr("allianceid")
+  rows.each do |r|
+    key = r.attr("solarsystemid")
+    val = r.attr("solarsystemname")
+    faction = r.attr("factionid")
+    alliance = r.attr("allianceid")
 
-  hash = Hash.new
+    hash = Hash.new
 
-  hash[key] = val
-  if alliance == "0" then hash["faction"] = "Empire"; else hash["faction"] = alliance; end
+    hash[key] = val
+    if alliance == "0" then hash["faction"] = "Empire"; else hash["faction"] = alliance; end
+    systemlist << hash
+  end
 
-  systemlist << hash
+  return systemlist
 end
-
-puts systemlist.to_json
-
