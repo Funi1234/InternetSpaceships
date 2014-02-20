e54037188c3cf7c6d6a87c36a856d4966d02ca84
David Kirwan <dkirwan@tssg.org>
2014-02-20 16:59:46 +0000

    - asdf

--- ruby/sqlite/constellation-systems.rb
+++ ruby/sqlite/constellation-systems.rb
@@ -7,9 +7,9 @@ begin
   rs = stm.execute
 
   rs.each do |row|
-    #puts row.inspect
-    puts "Constellation: " + row[1].to_s + " SolarSystemID: " + row[2].to_s + " Name: " + row[3].to_s + " X: " + "%4.2f" % (row[4] / 1.0e+15).to_s + 
-    " Y: " + "%4.2f" % (row[5] / 1.0e+15).to_s + " Z: " + "%4.2f" % (row[6] / 1.0e+15).to_s + " Sec Status: " + row[13].to_s
+    puts row.inspect
+    puts "Constellation: " + row[1].to_s + " SolarSystemID: " + row[2].to_s + " Name: " + row[3].to_s + " X: " + "%0.2f" % (row[4] / 1.0e+15).to_s + 
+    " Y: " + "%0.2f" % (row[5] / 1.0e+15).to_s + " Z: " + "%0.2f" % (row[6] / 1.0e+15).to_s + " Sec Status: " + row[13].to_s
   end
 
 rescue SQLite3::Exception => e 
