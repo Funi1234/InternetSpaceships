8f5eb04db849525ac5c5e70c9c2530847bffea91
David Kirwan <dkirwan@tssg.org>
2014-02-20 16:46:37 +0000

    - Added sample means to get the solar system IDs of each solar system with a gate linking to the target solar system

--- ruby/sqlite/constellation-systems.rb
+++ ruby/sqlite/constellation-systems.rb
@@ -8,7 +8,7 @@ begin
 
   rs.each do |row|
     #puts row.inspect
-    puts "Constellation: " + row[1].to_s + " Name: " + row[3].to_s + " X: " + "%4.2f" % (row[4] / 1.0e+15).to_s + 
+    puts "Constellation: " + row[1].to_s + " SolarSystemID: " + row[2].to_s + " Name: " + row[3].to_s + " X: " + "%4.2f" % (row[4] / 1.0e+15).to_s + 
     " Y: " + "%4.2f" % (row[5] / 1.0e+15).to_s + " Z: " + "%4.2f" % (row[6] / 1.0e+15).to_s + " Sec Status: " + row[13].to_s
   end
 
