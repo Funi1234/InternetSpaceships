be30a6d28ba7f6163ae5dc6b114c53f1ba5e09c4
David Kirwan <dkirwan@tssg.org>
2014-02-21 22:56:27 +0000

    - Added means to remove duplicate gate links, will come in handy when graphing

--- ruby/sqlite/remove_duplicate_jumps.rb
+++ ruby/sqlite/remove_duplicate_jumps.rb
@@ -4,4 +4,19 @@ f = File.open("../../static_dumps/eve_systems_jump_list.json", "r")
 jumps = JSON.parse(f.read)
 f.close
 
-puts jumps.size
+jump_list = Array.new
+
+jumps.each do |key, i|
+  i.each do |j|
+    jump_list.push([key, j])
+  end
+end
+
+
+jump_list.each do |i|
+  jump_list.delete(i.reverse)
+end
+
+g = File.open("../../static_dumps/eve_systems_jump_list_minus_duplicates.json", "w")
+g.write(JSON.pretty_generate(jump_list))
+g.close
